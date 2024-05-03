b112bfe8ebd11d0a58a70027d8a8825592a25a27
Nirav Thakar <t.nirav888@gmail.com>
2024-05-02 23:06:15 -0500

    Added Database connection string using mongoose and setup database connection

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,15 +1,17 @@
+const asyncHandler = require("express-async-handler");
+
 //@desc Get all Contacts
 //@route GET /api/contacts
 //@access public
-const getAllContacts = (req, res) => {
+const getAllContacts = asyncHandler((req, res) => {
     res.status(200).json({ message: "Get All Contacts" });
-}
+})
 
 
 //@desc Create a Contact
 //@route POST /api/contacts
 //@access public
-const createContact = (req,res) => {
+const createContact = asyncHandler((req,res) => {
     console.log("Request Body is: ", req.body);
     const {name,email,phone} = req.body;
     if(!name || !email || !phone){
@@ -18,27 +20,27 @@ const createContact = (req,res) => {
     }else{
         res.status(200).json({ message: "Create a Contact" });
     }
-}
+})
 
 //@desc Get a Contact
 //@route GET /api/contact/:id
 //@access public
-const getContact = (req, res) => {
+const getContact = asyncHandler((req, res) => {
     res.status(200).json({ message: `Get a Contact ${req.params.id} ` });
-}
+})
 
 //@desc Update a Contact
 //@route PUT /api/contact/:id
 //@access public
-const updateContact = (req, res) => {
+const updateContact = asyncHandler((req, res) => {
     res.status(200).json({ message: `Update a Contact ${req.params.id} ` });
-}
+})
 
 //@desc Delete a Contact
 //@route Delete /api/contact/:id
 //@access public
-const deleteContact = (req, res) => {
+const deleteContact = asyncHandler((req, res) => {
     res.status(200).json({ message: `Delete a Contact ${req.params.id} ` });
-}
+})
 
 module.exports = { getAllContacts,createContact,getContact,updateContact,deleteContact };
